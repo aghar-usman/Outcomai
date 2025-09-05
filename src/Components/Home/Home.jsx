be40c3d3e93f5bc8313353888e460171637b9e7c
Aghar Usman Kannanthodi <agharusman529@gmail.com>
2025-09-05 11:17:18 +0530

    Update Home.jsx

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import Hero from './Hero/Hero.jsx'
-import Features from './Features/Features.jsx'
-import Solution from './Solutions/Solution.jsx'
-import Outcomes from './Outcomes/Outcomes.jsx'
-import Proof from './Proof/Proof.jsx'
-import Cta from './Cta/Cta.jsx'
+import React from 'react';
+import Hero from './Hero/Hero';
+import Features from './Features/Features';
+import Solution from './Solutions/Solution';
+import Outcomes from './Outcomes/Outcomes';
+import Proof from './Proof/Proof';
+import Cta from './Cta/Cta';
 
 export const Home = () => {
   return (
-    <div>
-      <section id='Hero'> <Hero /> </section>
-      <section id='Features'> <Features /> </section>
-      <section id='Solution'> <Solution /> </section>
-      <section id='Outcomes'> <Outcomes /> </section>
-      <section id='Proof'> <Proof /> </section>
-      <section id='Cta'> <Cta /> </section>
+    <div className="home-container">
+      <section id="hero"><Hero /></section>
+      <section id="features"><Features /></section>
+      <section id="solution"><Solution /></section>
+      <section id="outcomes"><Outcomes /></section>
+      <section id="proof"><Proof /></section>
+      <section id="cta"><Cta /></section>
     </div>
-  )
-}
+  );
+};
 
 export default Home;
