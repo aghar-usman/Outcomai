2d3e83354ac8df818a2fac9cbb351a1c70358a89
Aghar Usman Kannanthodi <agharusman529@gmail.com>
2025-09-05 10:58:13 +0530

    Update navbar.jsx

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
-import './Navbar.css';
+import './navbar.css';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
